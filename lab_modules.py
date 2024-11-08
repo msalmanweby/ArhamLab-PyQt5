650ec35033fba582b570dca78c931df5b18e06e0
Muhammad Windows <msalman2339@gmail.com>
2024-11-08 22:35:27 +0500

    Update

--- lab_modules.py
+++ lab_modules.py
@@ -7,18 +7,21 @@ tests_list = [
                 "minValue": "60",
                 "maxValue": "120",
                 "unit": "mg/dl",
+                "price": "200"
             },
             {
                 "name": "Postprandial",
                 "minValue": "100",
                 "maxValue": "140",
                 "unit": "mg/dl",
+                "price": "1200"
             },
             {
                 "name": "Random",
                 "minValue": "70",
                 "maxValue": "140",
                 "unit": "mg/dl",
+                "price": "1000"
             },
         ],
     },
@@ -30,30 +33,35 @@ tests_list = [
                 "minValue": "13.5",
                 "maxValue": "17.5",
                 "unit": "g/dl",
+                "price": "1500"
             },
             {
                 "name": "White Blood Cell (WBC)",
                 "minValue": "4500",
                 "maxValue": "11000",
                 "unit": "cells/mcL",
+                "price": "800"
             },
             {
                 "name": "Platelets",
                 "minValue": "150000",
                 "maxValue": "450000",
                 "unit": "cells/mcL",
+                "price": "1200"
             },
             {
                 "name": "Red Blood Cell (RBC)",
                 "minValue": "4.7",
                 "maxValue": "6.1",
                 "unit": "million cells/mcL",
+                "price": "1000"
             },
             {
                 "name": "Hematocrit",
                 "minValue": "38.3",
                 "maxValue": "48.6",
                 "unit": "%",
+                "price": "1300"
             },
         ],
     },
@@ -65,24 +73,28 @@ tests_list = [
                 "minValue": "125",
                 "maxValue": "200",
                 "unit": "mg/dl",
+                "price": "700"
             },
             {
                 "name": "HDL (Good Cholesterol)",
                 "minValue": "40",
                 "maxValue": "60",
                 "unit": "mg/dl",
+                "price": "500"
             },
             {
                 "name": "LDL (Bad Cholesterol)",
                 "minValue": "0",
                 "maxValue": "100",
                 "unit": "mg/dl",
+                "price": "900"
             },
             {
                 "name": "Triglycerides",
                 "minValue": "0",
                 "maxValue": "150",
                 "unit": "mg/dl",
+                "price": "600"
             },
         ],
     },
@@ -94,30 +106,35 @@ tests_list = [
                 "minValue": "7",
                 "maxValue": "56",
                 "unit": "U/L",
+                "price": "800"
             },
             {
                 "name": "AST (SGOT)",
                 "minValue": "5",
                 "maxValue": "40",
                 "unit": "U/L",
+                "price": "650"
             },
             {
                 "name": "Alkaline Phosphatase",
                 "minValue": "44",
                 "maxValue": "147",
                 "unit": "U/L",
+                "price": "900"
             },
             {
                 "name": "Bilirubin Total",
                 "minValue": "0.1",
                 "maxValue": "1.2",
                 "unit": "mg/dl",
+                "price": "700"
             },
             {
                 "name": "Albumin",
                 "minValue": "3.5",
                 "maxValue": "5.0",
                 "unit": "g/dl",
+                "price": "1200"
             },
         ],
     },
@@ -129,30 +146,35 @@ tests_list = [
                 "minValue": "7",
                 "maxValue": "20",
                 "unit": "mg/dl",
+                "price": "600"
             },
             {
                 "name": "Creatinine",
                 "minValue": "0.6",
                 "maxValue": "1.3",
                 "unit": "mg/dl",
+                "price": "1000"
             },
             {
                 "name": "Uric Acid",
                 "minValue": "3.4",
                 "maxValue": "7.0",
                 "unit": "mg/dl",
+                "price": "700"
             },
             {
                 "name": "Sodium",
                 "minValue": "135",
                 "maxValue": "145",
                 "unit": "mmol/L",
+                "price": "800"
             },
             {
                 "name": "Potassium",
                 "minValue": "3.5",
                 "maxValue": "5.1",
                 "unit": "mmol/L",
+                "price": "500"
             },
         ],
     },
--- main.py
+++ main.py
@@ -20,9 +20,10 @@ from PyQt5.QtWidgets import (
     QDesktopWidget,
     QMessageBox,
     QTableWidgetItem, 
-    QCheckBox
+    QCheckBox,
+    QLabel
 )
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
 import sqlite3
 from lab_modules import tests_list
@@ -69,17 +70,18 @@ class MainWindow(QMainWindow):
                 registration_center TEXT,
                 specimen TEXT,
                 consultant_name TEXT,
+                scheduled_tests TEXT,
                 test_results TEXT
             )
         """)
         self.conn.commit()
 
     def create_pages(self):
-        self.add_entry_page = QStackedWidget()
+        # self.add_entry_page = QWidget()
         self.create_patient_info_form()
-        self.create_test_results_form()
+        # self.create_test_results_form()
 
-        self.pages.addWidget(self.add_entry_page)
+        # self.pages.addWidget(self.add_entry_page)
 
         # Add the other pages to the QStackedWidget
                
@@ -89,42 +91,143 @@ class MainWindow(QMainWindow):
 
     def create_patient_info_form(self):
         self.patient_info_form = QWidget()
-        form_layout = QFormLayout(self.patient_info_form)
+        main_layout = QVBoxLayout(self.patient_info_form)
 
-        # Add input fields
+        # obj = {
+        #     "patient_name" : QLineEdit()
+        # }
+
+        # Form layout for input fields
+        form_layout = QFormLayout()
+        action_buttons_layout = QHBoxLayout()
+
+        row_layout_0 = QHBoxLayout()
+        # First row (Patient Name and Father/Husband Name)
+        row_layout_1 = QHBoxLayout()
+        # Second row (Age, Gender, Nic, Address)
+        row_layout_2 = QHBoxLayout()
+        # Third row (Registration Center, Specimen, Consultant Name)
+        row_layout_3 = QHBoxLayout()
+        # Fourth row (Test Name, Test Result, Price)
+        row_layout_4 = QHBoxLayout()
+
+        page_label = QLabel("Add Patient Info")
+        reset_entry = QPushButton("Clear Entries")
+        reset_entry.setFixedWidth(100)
+        reset_entry.clicked.connect(self.reset_patient_info)
+
+        self.patient_name_label = QLabel("Patient Name:")
         self.patient_name = QLineEdit()
-        form_layout.addRow("Patient Name:", self.patient_name)
 
+        self.father_husband_name_label = QLabel("Father/Husband Name:")
         self.father_husband_name = QLineEdit()
-        form_layout.addRow("Father/Husband Name:", self.father_husband_name)
 
+        self.age_label = QLabel("Age:")
         self.age = QLineEdit()
-        form_layout.addRow("Age:", self.age)
 
+        self.gender_label = QLabel("Gender:")
         self.gender = QComboBox()
+        self.gender.setFixedWidth(200)
         self.gender.addItems(["Male", "Female", "Other"])
-        form_layout.addRow("Gender:", self.gender)
 
+        self.nic_number_label = QLabel("NIC Number:")
         self.nic_number = QLineEdit()
-        form_layout.addRow("NIC Number:", self.nic_number)
 
+        self.address_label = QLabel("Address:")
         self.address = QLineEdit()
-        form_layout.addRow("Address:", self.address)
 
+        self.registration_center_label = QLabel("Registration Center:")
         self.registration_center = QLineEdit()
-        form_layout.addRow("Registration Center:", self.registration_center)
 
-        self.specimen = QLineEdit()
-        form_layout.addRow("Specimen:", self.specimen)
+        self.specimen_label = QLabel("Specimen:")
+        self.specimen = QComboBox()
+        self.specimen.setFixedWidth(200)
+        self.specimen.addItems(["","Brought to Lab"])
 
-        self.consultant_name = QLineEdit()
-        form_layout.addRow("Consultant Name:", self.consultant_name)
+        self.consultant_name_label = QLabel("Consultant Name:")
+        self.consultant_name = QComboBox()
+        self.consultant_name.setFixedWidth(200)
+        self.consultant_name.addItems(["","Hamza"])
 
-        proceed_button = QPushButton("Proceed")
-        proceed_button.clicked.connect(self.create_entry_into_db)
-        form_layout.addRow(proceed_button)
+        self.test_name_label = QLabel("Test Name:")
+        self.test_name = QComboBox()
+        self.test_name.setFixedWidth(400)
+        self.test_name.addItems(test["name"] for test in tests_list)
 
-        self.add_entry_page.addWidget(self.patient_info_form)
+        self.test_type_label = QLabel("Test Type:")
+        self.test_type = QComboBox()
+        self.test_type.setFixedWidth(400)
+        self.update_test_types()
+        self.test_name.currentIndexChanged.connect(self.update_test_types)
+
+        self.price_label = QLabel("Price:")
+        self.price = QLineEdit()
+        self.price.setReadOnly(True)
+        self.update_test_price()
+        self.test_type.currentIndexChanged.connect(self.update_test_price)
+        self.scheduled_tests = []
+
+        row_layout_0.addWidget(page_label)
+        row_layout_0.addWidget(reset_entry)
+
+        row_layout_1.addWidget(self.patient_name_label)
+        row_layout_1.addWidget(self.patient_name)
+        row_layout_1.addWidget(self.father_husband_name_label)
+        row_layout_1.addWidget(self.father_husband_name)
+        row_layout_1.addWidget(self.nic_number_label)
+        row_layout_1.addWidget(self.nic_number)
+        form_layout.addRow(row_layout_1)
+
+        row_layout_2.addWidget(self.age_label)
+        row_layout_2.addWidget(self.age)
+        row_layout_2.addWidget(self.gender_label)
+        row_layout_2.addWidget(self.gender)
+        row_layout_2.addWidget(self.address_label)
+        row_layout_2.addWidget(self.address)
+        form_layout.addRow(row_layout_2)
+
+        row_layout_3.addWidget(self.registration_center_label)
+        row_layout_3.addWidget(self.registration_center)
+        row_layout_3.addWidget(self.specimen_label)
+        row_layout_3.addWidget(self.specimen)
+        row_layout_3.addWidget(self.consultant_name_label)
+        row_layout_3.addWidget(self.consultant_name)
+        form_layout.addRow(row_layout_3)
+
+        row_layout_4.addWidget(self.test_name_label)
+        row_layout_4.addWidget(self.test_name)
+        row_layout_4.addWidget(self.test_type_label)
+        row_layout_4.addWidget(self.test_type)
+        row_layout_4.addWidget(self.price_label)
+        row_layout_4.addWidget(self.price)
+        form_layout.addRow(row_layout_4)
+
+        add_entry = QPushButton("Add Entry")
+        add_entry.clicked.connect(self.add_results_in_table)
+
+        remove_entry = QPushButton("Remove Entry")
+        remove_entry.clicked.connect(self.remove_results_from_table)
+
+        save_record = QPushButton("Save Record")
+        save_record.clicked.connect(self.create_entry_into_db)
+        
+        action_buttons_layout.addWidget(add_entry)
+        action_buttons_layout.addWidget(remove_entry)
+        action_buttons_layout.addWidget(save_record)
+        form_layout.addRow(action_buttons_layout)
+
+        # Add form layout to the main layout
+        main_layout.addLayout(row_layout_0)
+        main_layout.addLayout(form_layout)
+
+        self.schedule_test_table = QTableWidget()
+        self.schedule_test_table.setColumnCount(4)
+        self.schedule_test_table.setHorizontalHeaderLabels(["Select", "Test Name", "Test Type", "Price"])
+        header = self.schedule_test_table.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.Stretch)
+        main_layout.addWidget(self.schedule_test_table)
+
+        self.pages.addWidget(self.patient_info_form)
 
     def create_test_results_form(self):
         self.test_results_form = QWidget()
@@ -166,9 +269,6 @@ class MainWindow(QMainWindow):
         self.update_test_measurements()
         self.test_type.currentIndexChanged.connect(self.update_test_measurements)
 
-        # Action buttons
-        # backward_button = QPushButton("Previous")
-        # backward_button.clicked.connect(lambda: self.swap_generate_report_forms(index=0))
 
         add_record = QPushButton("Add Record")
         add_record.clicked.connect(self.add_results_in_table)
@@ -185,12 +285,12 @@ class MainWindow(QMainWindow):
         main_layout.addLayout(form_layout)
 
         # Create and add the QTableWidget outside the form layout
-        self.test_results_table = QTableWidget()
-        self.test_results_table.setColumnCount(7)
-        self.test_results_table.setHorizontalHeaderLabels(["Select", "Test Name", "Test Type", "Min Value", "Max Value", "Unit", "Result"])
-        header = self.test_results_table.horizontalHeader()
+        self.schedule_test_table = QTableWidget()
+        self.schedule_test_table.setColumnCount(7)
+        self.schedule_test_table.setHorizontalHeaderLabels(["Select", "Test Name", "Test Type", "Min Value", "Max Value", "Unit", "Result"])
+        header = self.schedule_test_table.horizontalHeader()
         header.setSectionResizeMode(QHeaderView.Stretch)
-        main_layout.addWidget(self.test_results_table)  # Add the table as a widget
+        main_layout.addWidget(self.schedule_test_table)  # Add the table as a widget
 
         delete_button = QPushButton("Remove Records")
         delete_button.clicked.connect(self.remove_results_from_table)
@@ -209,8 +309,12 @@ class MainWindow(QMainWindow):
             # Show error message if patient name is empty
             self.show_error_dialog("Error", "Patient name cannot be empty!")
             return
+        if not self.scheduled_tests:
+            # Show error message if patient name is empty
+            self.show_error_dialog("Error", "No entries found for Scheduled test")
+            return
         
-        self.test_results_list = []
+        
 
         patient_data = {
             "patient_name": self.patient_name.text(),
@@ -221,21 +325,29 @@ class MainWindow(QMainWindow):
             "address": self.address.text(),
             "registration_date": datetime.now(self.time_zone).strftime('%Y-%m-%d %H:%M:%S'),
             "registration_center": self.registration_center.text(),
-            "specimen": self.specimen.text(),
-            "consultant_name": self.consultant_name.text(),
-            "test_results": json.dumps(self.test_results_list),  # JSON data as string
+            "specimen": self.specimen.currentText(),
+            "consultant_name": self.consultant_name.currentText(),
+            "scheduled_tests": json.dumps(self.scheduled_tests),  # JSON data as string
+            "test_results": json.dumps([]),  # JSON data as string
         }
-        self.cursor.execute("""
-            INSERT INTO LabReport (
-                patient_name, father_husband_name, age, gender, nic_number,
-                address, registration_date, registration_center, specimen,
-                consultant_name, test_results
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """, tuple(patient_data.values()))
-        self.conn.commit()
-        if self.cursor.lastrowid:
-            self.case_no.setText(str(self.cursor.lastrowid))
-            self.swap_generate_report_forms(index=1)
+        try:
+            self.cursor.execute("""
+                INSERT INTO LabReport (
+                    patient_name, father_husband_name, age, gender, nic_number,
+                    address, registration_date, registration_center, specimen,
+                    consultant_name, scheduled_tests, test_results
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, tuple(patient_data.values()))
+            self.conn.commit()
+            if self.cursor.lastrowid:
+                self.show_error_dialog("Success", f"Case {self.cursor.lastrowid} registration successfull!")
+        except Exception as e:
+            self.show_error_dialog("Error", "Something went wrong, Try Again!")
+            print(e)
+            return
+        # if self.cursor.lastrowid:
+        #     self.case_no.setText(str(self.cursor.lastrowid))
+        #     self.swap_generate_report_forms(index=1)
 
     def update_entry_into_db(self):
         """Save the test results added into test results list into db"""
@@ -266,7 +378,20 @@ class MainWindow(QMainWindow):
             if test["name"] == selected_test_name:
                 self.test_type.addItems(test_type["name"] for test_type in test["types"])
                 break
-    
+
+    def update_test_price(self):
+        """Update the test price options the test type changes""" 
+        self.price.clear()
+        selected_test_name = self.test_name.currentText()
+        selected_test_type = self.test_type.currentText()
+
+        for test in tests_list:
+            if test["name"] == selected_test_name:
+                for test_type in test["types"]:
+                    if test_type["name"] == selected_test_type:
+                        self.price.setText(test_type["price"])
+                        break
+
     def update_test_measurements(self):
         """Update min_value, max_value and unit when the test type changes"""
         self.min_value.clear()
@@ -288,25 +413,18 @@ class MainWindow(QMainWindow):
 
     def add_results_in_table(self):
         """Add the done test results in the table to view"""
-        if not self.result.text().strip():
-            # Show error message if the result field is empty
-            self.show_error_dialog("Error", "Please enter the test results")
-            return
 
-        test_result = {
+        test = {
             "name": self.test_name.currentText(),
             "type": self.test_type.currentText(),
-            "minValue": self.min_value.text(),
-            "maxValue": self.max_value.text(),
-            "unit": self.unit.text(),
-            "result": self.result.text()
+            "price": self.price.text(),
         }
 
-        self.test_results_list.append(test_result)
-        row_position = self.test_results_table.rowCount()
+        self.scheduled_tests.append(test)
+        row_position = self.schedule_test_table.rowCount()
 
         # Insert a new row
-        self.test_results_table.insertRow(row_position)
+        self.schedule_test_table.insertRow(row_position)
 
         # Create a checkbox widget for row selection (first column)
         checkbox_widget = QWidget()
@@ -315,35 +433,32 @@ class MainWindow(QMainWindow):
         checkbox_layout.addWidget(checkbox)
         checkbox_layout.setAlignment(Qt.AlignCenter)  # Center the checkbox in the cell
         checkbox_layout.setContentsMargins(0, 0, 0, 0)  # Remove padding
-        self.test_results_table.setCellWidget(row_position, 0, checkbox_widget)
+        self.schedule_test_table.setCellWidget(row_position, 0, checkbox_widget)
 
         # Set the checkbox column width
-        self.test_results_table.setColumnWidth(0, 30)
+        self.schedule_test_table.setColumnWidth(0, 30)
 
         # Insert the test data in the remaining columns (starting from column 1)
-        self.test_results_table.setItem(row_position, 1, QTableWidgetItem(test_result["name"]))
-        self.test_results_table.setItem(row_position, 2, QTableWidgetItem(test_result["type"]))
-        self.test_results_table.setItem(row_position, 3, QTableWidgetItem(test_result["minValue"]))
-        self.test_results_table.setItem(row_position, 4, QTableWidgetItem(test_result["maxValue"]))
-        self.test_results_table.setItem(row_position, 5, QTableWidgetItem(test_result["unit"]))
-        self.test_results_table.setItem(row_position, 6, QTableWidgetItem(test_result["result"]))
+        self.schedule_test_table.setItem(row_position, 1, QTableWidgetItem(test["name"]))
+        self.schedule_test_table.setItem(row_position, 2, QTableWidgetItem(test["type"]))
+        self.schedule_test_table.setItem(row_position, 3, QTableWidgetItem(test["price"]))
 
         # Set text alignment for all columns with test data
-        for column in range(1, self.test_results_table.columnCount()):
-            item = self.test_results_table.item(row_position, column)
+        for column in range(1, self.schedule_test_table.columnCount()):
+            item = self.schedule_test_table.item(row_position, column)
             if item:
                 item.setTextAlignment(Qt.AlignCenter)
 
         # Optionally, clear the input fields after adding the result to the table
-        self.result.clear()
+        # self.result.clear()
 
     def remove_results_from_table(self):
         """Delete rows that are selected via checkboxes"""
         rows_to_delete = []
         
         # Check which rows are selected for deletion
-        for row in range(self.test_results_table.rowCount()):
-            checkbox_widget = self.test_results_table.cellWidget(row, 0)  # The checkbox is in the first column
+        for row in range(self.schedule_test_table.rowCount()):
+            checkbox_widget = self.schedule_test_table.cellWidget(row, 0)  # The checkbox is in the first column
             if checkbox_widget is not None:
                 checkbox = checkbox_widget.findChild(QCheckBox)
                 if checkbox and checkbox.isChecked():
@@ -357,14 +472,41 @@ class MainWindow(QMainWindow):
         # Delete selected rows in reverse order to avoid index shifting
         for row in reversed(rows_to_delete):
             # Remove from test_results_list as well
-            del self.test_results_list[row]
+            del self.scheduled_tests[row]
 
             # Remove the row from the table
-            self.test_results_table.removeRow(row)
+            self.schedule_test_table.removeRow(row)
+
+    def reset_patient_info(self):
+        """Reset scheduled tests and clear the table"""
+
+        # Clear the list of scheduled tests
+        self.scheduled_tests = []  # Reset the list of scheduled tests
+
+        # Clear all rows in the table (except for the headers)
+        row_count = self.schedule_test_table.rowCount()
+        for row in range(row_count):
+            self.schedule_test_table.removeRow(0)  # Remove each row starting from the top
+            
+        self.test_name.setCurrentIndex(0)
+
+        # Optional: You can also reset any other widgets related to scheduled tests
+        self.patient_name.setText("")
+        self.father_husband_name.setText("")
+        self.age.setText("")
+        self.gender.setCurrentIndex(0)
+        self.nic_number.setText("")
+        self.address.setText("")
+        self.registration_center.setText("")
+        self.specimen.setCurrentIndex(0)
+        self.consultant_name.setCurrentIndex(0)
+        self.test_name.setCurrentIndex(0)
+
 
     def show_error_dialog(self, title, message):
         """Shows an error message box with the given title and message."""
         error_dialog = QMessageBox()
+        error_dialog.setFixedWidth(400)
         if title == "Error":
             error_dialog.setIcon(QMessageBox.Critical)
         else:
